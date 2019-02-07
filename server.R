db10fce42fa10778ecd990f64b220785d91127f0
fabrice zaoui <fabrice.zaoui@edf.fr>
2019-02-07 17:56:05 +0100

    first commit with default files for a shiny app

---
+++ server.R
@@ -0,0 +1,13 @@
+library(shiny)
+library(plotly)
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+  output$Test <- renderPlotly({
+    x <- c(1:100)
+    random_y <- rnorm(100, mean = 0)
+    data <- data.frame(x, random_y)
+    p <- plot_ly(data, x = ~x, y = ~random_y, type = 'scatter', mode = 'lines')
+  })
+})
---
+++ ui.R
@@ -0,0 +1,7 @@
+library(shiny)
+library(plotly)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+      plotlyOutput("Test")
+))
