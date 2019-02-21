30fbacaa957d4e86900ed8530aa8579e1b6642ef
fabrice zaoui <fabrice.zaoui@edf.fr>
2019-02-21 18:25:37 +0100

    dynamic table length

--- server.R
+++ server.R
@@ -22,10 +22,8 @@ library(plotly)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
 
-  projectmodel <- read.xlsx2('model/projet.xlsx', sheetIndex = 1, header = FALSE)
-  projectmodel <- cbind(projectmodel, rep(NA, dim(projectmodel)[1]))
-  colnames(projectmodel) <- NULL
-  tempmodel <- projectmodel[4:10,]
+  projectmodel <- read.xlsx2('model/projet.xlsx', sheetIndex = 1, header = TRUE, stringsAsFactors = FALSE)
+  prj <- projectmodel[4:10,]
   
   observeEvent(input$redir1, {
     updateTabsetPanel(session, "tabs", selected = "projet")
@@ -59,10 +57,18 @@ shinyServer(function(input, output, session) {
     projectmodel[1,2] <- input$projectname
     projectmodel[2,2] <- input$projectcontext
     projectmodel[3,2] <- input$sitenumber
-    #for(i in 2:input$sitenumber){
-    #  projectmodel <- rbind(projectmodel, tempmodel)
-    #}
-    DT::datatable(projectmodel)
+    # append new fiels if necessary
+    if(!is.na(input$sitenumber)){
+      nsite <- input$sitenumber
+      nprj <- dim(prj)[1]
+      nproj <- dim(projectmodel)[1] - 3
+      niter <- ((nsite * nprj) - nproj) / nprj
+      if(niter > 0){
+        for(i in 1:niter){
+          projectmodel <- rbind(projectmodel, prj)
+        }
+      }
+      DT::datatable(projectmodel[4:dim(projectmodel)[1],], options = list(pageLength = 7, autoWidth = TRUE), rownames = FALSE)
+    }
   })
-
 })
